6eaad60bb17e0d68f440a88f396c08df7e3a8e54
abdi01dev <abdi01dev@gmail.com>
2024-03-15 11:18:10 +0700

    add: menambahkan section testimonial company

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,13 @@ hamburgerToggle.addEventListener("click", () => {
   hamburgerToggle.classList.toggle("active");
   navbarExtra.classList.toggle("active");
 });
+
+// Company Logo Scroller
+const scroller = document.querySelector(".scroller");
+const scrollerInner = scroller.querySelector(".scroller__inner");
+const scrollerContent = Array.from(scrollerInner.children);
+
+scrollerContent.forEach((item) => {
+  const duplicatedItem = item.cloneNode(true);
+  scrollerInner.appendChild(duplicatedItem);
+});
